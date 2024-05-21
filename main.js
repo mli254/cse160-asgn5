24d4b3d1ed14cee6f2b0fc07011ddff2f1006d70
mli254 <mli254@ucsc.edu>
2024-05-21 01:49:29 -0700

    5b requirements

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
 import {MTLLoader} from 'three/addons/loaders/MTLLoader.js';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
 function main() {
 
@@ -11,22 +12,82 @@ function main() {
 
     // Setting up Camera
 	const fov = 75;
-	const aspect = 1; // the canvas default
+	const aspect = 2; // the canvas default
 	const near = 0.1;
-	const far = 10;
+	const far = 100;
 	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-	camera.position.z = 5;
+	camera.position.set(0, 0, 5);
+
+    const controls = new OrbitControls(camera, canvas);
+    controls.target.set(0, 0, 0);
+    controls.update();
 
 	const scene = new THREE.Scene(); // root of screen graph
     scene.background = new THREE.Color( 0x4c7857 );
 
-    // Setting up Lighting
+    // Setting up a CubeMap
     {
-        const color = 0xFFFFFF;
+        const loader = new THREE.CubeTextureLoader();
+        const texture = loader.load([
+            './images/quarry_cubemap_right.png',
+            './images/quarry_cubemap_left.png',
+            './images/quarry_cubemap_top.png',
+            './images/quarry_cubemap_bottom.png',
+            './images/quarry_cubemap_front.png',
+            './images/quarry_cubemap_back.png'
+        ]);
+        scene.background = texture;
+    }
+
+    // Setting up Directional Lighting
+    {
+        const color = 0xc0d1ed;
         const intensity = 5;
         const light = new THREE.DirectionalLight( color, intensity );
-        light.position.set( - 1, 2, 4 );
-        scene.add( light );
+        light.position.set(-2, -4, 2);
+        light.target.position.set(5, 0, 0);
+        scene.add(light);
+        scene.add(light.target);
+
+        const helper = new THREE.DirectionalLightHelper(light);
+        scene.add(helper);
+    }
+
+    // Hemisphere Light
+    {
+        const skyColor = 0xB1E1FF;  // light blue
+        const groundColor = 0xB97A20;  // brownish orange
+        const intensity = 1;
+        const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+        scene.add(light);
+    }
+
+    // Point Light
+    {
+        const color = 0xedc0c1;
+        const intensity = 150;
+        const light = new THREE.PointLight(color, intensity);
+        light.position.set(0, 5, 0);
+        scene.add(light);
+
+        const helper = new THREE.PointLightHelper(light);
+        scene.add(helper);
+    }
+
+    // Spot Light
+    {
+        const color = 0xFFFFFF;
+        const intensity = 250;
+        const angle = THREE.MathUtils.radToDeg(30);
+        const penumbra = 0.5;
+        const light = new THREE.SpotLight(color, intensity, 0, angle, penumbra);
+        light.position.set(5, -4, 2);
+        light.target.position.set(-5, 0, 0);
+        scene.add(light);
+        scene.add(light.target);
+
+        const helper = new THREE.SpotLightHelper(light);
+        scene.add(helper);
     }
 
     // Adding a model
@@ -54,17 +115,30 @@ function main() {
     const radialSegments = 12;
     const cylinderGeometry = new THREE.CylinderGeometry( radiusTop, radiusBottom, cylinderHeight, radialSegments );
 
+    // Cone Geometry Settings
     const coneRadius = 1;
     const coneHeight = 1;
     const coneSegments = 16;
     const coneGeometry = new THREE.ConeGeometry( coneRadius, coneHeight, coneSegments);
 
+    // Plane Geometry Settings
+    const planeSize = 40;
+    const planeGeometry = new THREE.PlaneGeometry(planeSize, planeSize);
+
+    // Sphere Geometry Settings
+    const sphereRadius = 0.25;
+    const sphereWidthDivisions = 32;
+    const sphereHeightDivisions = 16;
+    const sphereGeometry = new THREE.SphereGeometry(sphereRadius, sphereWidthDivisions, sphereHeightDivisions);
+
+
     // Textures Settings
     const loader = new THREE.TextureLoader();
-	const texture = loader.load( './images/miku.png' );
-	texture.colorSpace = THREE.SRGBColorSpace;
+	const mikuTexture = loader.load( './images/miku.png' );
+	mikuTexture.colorSpace = THREE.SRGBColorSpace;
 
     const objects = [];
+    const snowObjects = [];
 
     // Create Color Material
     function createColorMaterial(color) {
@@ -92,6 +166,16 @@ function main() {
 
     }
 
+    function addSnow(amount) {
+        for (let i = 0; i < amount; i++) {
+            const mesh = new THREE.Mesh(sphereGeometry, createColorMaterial(0xffffff));
+            mesh.position.x = (i - amount/2)*5;
+            mesh.position.y = (Math.random()*10);
+            scene.add(mesh);
+            snowObjects.push(mesh);
+        }
+    }
+
     // Abstracted method for creating geometries
     function addSolidGeometry( x, y, geometry, material ) {
 
@@ -102,22 +186,46 @@ function main() {
 
     addSolidGeometry(-2, 0, cubeGeometry, createColorMaterial(0x8844aa));
     addSolidGeometry(2, 0, cylinderGeometry, createColorMaterial(0xaa8844));
-    addSolidGeometry(0, 2, coneGeometry, createTexturedMaterial(texture));
-    addSolidGeometry(0, -2, cubeGeometry, createTexturedMaterial(texture));
+    addSolidGeometry(0, 2, coneGeometry, createTexturedMaterial(mikuTexture));
+    addSolidGeometry(0, -2, cubeGeometry, createTexturedMaterial(mikuTexture));
+    const planeObj = new THREE.Mesh( planeGeometry, createTexturedMaterial(mikuTexture) );
+    planeObj.position.x = 0;
+    planeObj.position.y = -10;
+    planeObj.rotation.x = Math.PI * -.5;
+    scene.add(planeObj)
+
+    const sphereObj = new THREE.Mesh(sphereGeometry, createColorMaterial(0xffffff));
+    sphereObj.position.x = 0;
+    sphereObj.position.y = -7;
+    scene.add(sphereObj);
+
+    addSnow(20);
 
     // Animation
 	function render( time ) {
 
 		time *= 0.001; // convert time to seconds
+        let timePeriod = 5;
 
-		objects.forEach( ( obj, ndx ) => {
+		// objects.forEach( ( obj, ndx ) => {
 
-			const speed = 1 + ndx * .1;
-			const rot = time * speed;
-			obj.rotation.x = rot;
-			obj.rotation.y = rot;
+		// 	const speed = 1 + ndx * .1;
+		// 	const rot = time * speed;
+		// 	obj.rotation.x = rot;
+		// 	obj.rotation.y = rot;
 
-		} );
+		// } );
+
+        snowObjects.forEach( (obj, ndx)  => {
+            if (obj.position.y < -10) {
+                obj.position.y = 10;
+                timePeriod = time;
+            } else {
+                const speed = 1 + ndx * .001;
+                const delta = speed;
+                obj.position.y -= delta;
+            }
+        });
 
 		renderer.render( scene, camera );
 
